f070e2cc85f82594129d817a5541b32948476ca8
createaweb-tech <akifullah.tech@gmail.com>
2026-04-23 16:14:56 +0500

    Update route

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-    Route::get('/clear-cache', function () {
+    Route::get('/clear', function () {
         Artisan::call('config:clear');
         Artisan::call('route:clear');
         Artisan::call('cache:clear');
